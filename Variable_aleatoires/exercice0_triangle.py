0cf23164204152d5cc16ae536326118db0fd11f6
Antoine07 <antoine.lucsko@gmail.com>
2018-05-23 16:04:04 +0200

    affichage du triangle impair

--- Variable_aleatoires/exercice0_triangle.py
+++ Variable_aleatoires/exercice0_triangle.py
@@ -11,4 +11,15 @@ for i in range(1, N):
         triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
     triangle[i][i] = 1
 
-print(triangle)
+# print(triangle)
+
+for line in triangle:
+    for c in line:
+        if c == 0:
+            continue
+        if c % 2 == 1:
+            print('*', end='')
+        else:
+            print(' ', end='')
+
+    print()
